d869c24a94c036c5345f0b5ae5db1b6bd9dfdd0a
Maksymilian Łyszczarz <157590586+Anubis-max@users.noreply.github.com>
2024-07-14 20:56:23 +0200

    Add files via upload

--- js/slider.js
+++ js/slider.js
@@ -2,30 +2,43 @@ let slideIndex = 0;
 let slideInterval;
 let isPlaying = true;
 
-function showSlides() {
+function showSlides(n = null) {
     let i;
     let slides = document.getElementsByClassName("mySlides");
     let dots = document.getElementsByClassName("dot");
+
     for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";  
+        slides[i].style.display = "none";
     }
-    slideIndex++;
-    if (slideIndex > slides.length) {slideIndex = 1}    
+
+    if (n !== null) {
+        slideIndex = n;
+    } else {
+        slideIndex++;
+    }
+
+    if (slideIndex > slides.length) { slideIndex = 1; }
+    if (slideIndex < 1) { slideIndex = slides.length; }
+
     for (i = 0; i < dots.length; i++) {
         dots[i].className = dots[i].className.replace(" active", "");
     }
-    slides[slideIndex-1].style.display = "block";  
-    dots[slideIndex-1].className += " active";
-    slideInterval = setTimeout(showSlides, 2000);
+
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+
+    if (isPlaying) {
+        slideInterval = setTimeout(showSlides, 2000);
+    }
 }
 
 function toggleSlider() {
     let button = document.getElementById("sliderControlButton");
     if (isPlaying) {
-        clearTimeout(slideInterval); 
+        clearTimeout(slideInterval);
         button.textContent = "Play";
     } else {
-        showSlides(); 
+        showSlides();
         button.textContent = "Pause";
     }
     isPlaying = !isPlaying;
@@ -34,11 +47,11 @@ function toggleSlider() {
 function enterFullscreen(element) {
     if (element.requestFullscreen) {
         element.requestFullscreen();
-    } else if (element.mozRequestFullScreen) { 
+    } else if (element.mozRequestFullScreen) {
         element.mozRequestFullScreen();
-    } else if (element.webkitRequestFullscreen) { 
+    } else if (element.webkitRequestFullscreen) {
         element.webkitRequestFullscreen();
-    } else if (element.msRequestFullscreen) { 
+    } else if (element.msRequestFullscreen) {
         element.msRequestFullscreen();
     }
 }
@@ -46,9 +59,9 @@ function enterFullscreen(element) {
 function addFullscreenEvent() {
     let slides = document.getElementsByClassName("scaled-image");
     for (let slide of slides) {
-        slide.addEventListener('click', function() {
+        slide.addEventListener('click', function () {
             if (isPlaying) {
-                toggleSlider(); 
+                toggleSlider();
             }
             enterFullscreen(slide);
             slide.classList.add('fullscreen');
@@ -56,14 +69,22 @@ function addFullscreenEvent() {
     }
 }
 
-document.addEventListener("fullscreenchange", function() {
+function currentSlide(n) {
+    clearTimeout(slideInterval);
+    showSlides(slideIndex = n);
+    if (!isPlaying) {
+        toggleSlider();
+    }
+}
+
+document.addEventListener("fullscreenchange", function () {
     if (!document.fullscreenElement) {
         let slides = document.getElementsByClassName("scaled-image");
         for (let slide of slides) {
             slide.classList.remove('fullscreen');
         }
         if (!isPlaying) {
-            toggleSlider(); 
+            toggleSlider();
         }
     }
 });
